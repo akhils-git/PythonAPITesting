5a964d581a12328261483d6195e7cfda70467eeb
Akhil mon <akhilmon@hotmail.com>
2023-01-13 19:28:03 +0530

    Api updated

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
-from flask import Flask
+
+from flask import Flask, jsonify, request
 from datetime import datetime
 
 app = Flask(__name__)
@@ -9,21 +10,28 @@ print("Python Api Running...")
 @app.route("/")
 def hello():
     # print('Base called')
-    api_log("Base called")
+    api_log_save("Base", "Running Good")
     return "Hello, World!"
 
 
-@app.route("/api/getname/<name>")
+@app.route('/api/getname/<name>', methods=['GET'])
 def getname(name):
-    api_log("Getname called")
-    return "Hi" + name
+    api_log_save("getname", "Get name")
+    return jsonify("Hi " + name + " Called at " + str(datetime.now()))
 
 
-def api_log(text):
+@app.route('/api/getlog', methods=['GET'])
+def getlog():
+    api_log_save("getlog", "Log Saved")
+    logFile = open("log_file.txt", "r")
+    return jsonify(logFile.read())
+
+
+def api_log_save(api_name, message):
     logFile = open("log_file.txt", "a")  # append mode
-    logFile.write(text+" "+str(datetime.now())+"\n")
+    logFile.write(f"{api_name}|{message}|{str(datetime.now())}\n")
     logFile.close()
 
 
 # app.run(debug=True, port=5000)
-app.run(host='0.0.0.0', port=5000)
+app.run(host='0.0.0.0', port=9000)
