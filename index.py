10d38d986de77693f7d0fd418fe95510330b074b
Akhil mon <akhilmon@hotmail.com>
2023-02-27 16:59:54 +0530

    IMage detection not working

--- index.py
+++ index.py
@@ -5,12 +5,14 @@ from datetime import datetime
 from core.calibration_predictor import calibrationController
 from core.chat_gpt import chatGPTController
 from file_manage import FileController
+from core.image_processing import ImageProcessingController
 
 
 app = Flask(__name__)
 cal = calibrationController()
 file_controller = FileController()
 chatgpt_controller = chatGPTController()
+image_processing_controller = ImageProcessingController()
 
 print("Python Api Running...")
 
@@ -47,14 +49,15 @@ def upload_file():
     api_log_save("fileupload", "Called")
     return responce
 
-@app.route('/api/detectimageobjects', methods=['POST'])
-def upload_file():
-    print(request.files)
-    responce = file_controller.upload_file(request)
 
-    
-    api_log_save("fileupload", "Called")
-    return responce
+# @app.route('/api/detectimageobjects', methods=['POST'])
+# def detectimageobjects():
+#     print(request.files)
+#     responce = file_controller.upload_file(request)
+
+#     api_log_save("fileupload", "Called")
+#     return responce
+
 
 @app.route('/api/filelist', methods=['GET'])
 def filelist():
@@ -67,6 +70,14 @@ def filelist():
 def mongodb_quick_save():
     responce = file_controller.list_files()
     api_log_save("filelist", "Called")
+    return responce
+
+
+@app.route('/api/detectimageobjects', methods=['POST'])
+def detectimageobjects():
+    print(request.files)
+    responce = image_processing_controller.object_detection(request)
+    api_log_save("fileupload", "Called")
     return responce
 
 # @app.route("/api/trychatgpt/<qtn>")
