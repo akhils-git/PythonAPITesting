f64d1dd576fd674c7b338ee9eaaaa34808093389
Akhil mon <akhilmon@hotmail.com>
2023-09-27 16:37:50 +0530

    all download and single file download done

--- index.py
+++ index.py
@@ -1,11 +1,13 @@
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_file
 from datetime import datetime
 # from calibration_predictor import calibrationController
 from core.calibration_predictor import calibrationController
 from core.chat_gpt import chatGPTController
 from file_manage import FileController
 from core.image_processing import ImageProcessingController
+from zipfile import ZipFile
+import os
 
 
 app = Flask(__name__)
@@ -50,11 +52,50 @@ def upload_file():
     api_log_save("fileupload", "Called")
     return responce
 
+
 @app.route('/api/filelist', methods=['GET'])
 def filelist():
     responce = file_controller.list_files()
     api_log_save("filelist", "Called")
     return responce
+
+
+@app.route('/api/filedownload', methods=['GET'])
+def filedownload():
+   # Replace 'path_to_your_file' with the actual path to the file you want to send
+    file_path = 'storage/uploads/Car2.png'
+
+    # You can specify a custom filename for the client to save the file as (optional)
+    client_filename = 'Car2.png'
+
+    return send_file(file_path, as_attachment=True, download_name=client_filename)
+    return responce
+
+# @app.route('/api/filesdownload', methods=['GET'])
+# def filedownload():
+#    # Replace 'path_to_your_file' with the actual path to the file you want to send
+#     file_path = 'storage/uploads/Car2.png'
+
+#     # You can specify a custom filename for the client to save the file as (optional)
+#     client_filename = 'Car2.png'
+
+#     # Use send_file to send the file as a response
+#     return send_file(file_path, as_attachment=True, download_name=client_filename)
+#     return responce
+
+
+@app.route('/api/downloadallfromstorage')
+def download_all_from_storage():
+    # Create a temporary zip file to store the files
+    file_paths = os.listdir('storage/uploads/')
+    zip_filename = 'downloaded_files.zip'
+    with ZipFile(zip_filename, 'w') as zipf:
+        for file_path in file_paths:
+            # Add each file to the zip archive
+            zipf.write('storage/uploads/'+file_path,
+                       os.path.basename(file_path))
+    # Send the zip file as a response
+    return send_file(zip_filename, as_attachment=True, download_name='multiple_files.zip')
 
 
 @app.route('/api/mongodb_quick_save', methods=['GET'])
@@ -74,6 +115,7 @@ def detectimageobjects():
     api_log_save("getchatgptlog", "Log Saved")
     logFile = open("./storage/chat_gpt_log_file.txt", "r")
     return jsonify(logFile.read())
+
 
 def api_log_save(api_name, message):
     logFile = open("./storage/log_file.txt", "a")  # append mode
